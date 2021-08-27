5853ad2acb109379c2c7f7c5183d012cca35904e
ethan berk <48163588+EthanBerk@users.noreply.github.com>
2021-08-26 21:35:19 -0600

    MaigTime 2
    Did debuging work on property drawers

--- Assets/Editor/GunMods/pro.cs
+++ Assets/Editor/GunMods/pro.cs
@@ -11,22 +11,33 @@ namespace Editor.GunMods
         public override void OnGUI(Rect position, SerializedProperty property,
             GUIContent label)
         {
-           
+
+
+
             
-                property.FindPropertyRelative(nameof(StatEffect.Curve)).animationCurveValue = EditorGUILayout.CurveField(
-                    property.FindPropertyRelative(nameof(StatEffect.Curve)).animationCurveValue,
-                    Color.blue,
-                    new Rect(0f, property.FindPropertyRelative(nameof(StatEffect.LowerBound)).floatValue, 1,
-                        property.FindPropertyRelative(nameof(StatEffect.UpperBound)).floatValue -
-                        property.FindPropertyRelative(nameof(StatEffect.LowerBound)).floatValue));
+            EditorGUI.PropertyField(new Rect(position.x, position.y/4, position.width, position.height/4), property.FindPropertyRelative(nameof(StatEffect.Stat)));
+            EditorGUI.PropertyField(new Rect(position.x, position.y/2, position.width, position.height/4), property.FindPropertyRelative(nameof(StatEffect.LowerBound)));
+            EditorGUI.PropertyField(new Rect(position.x, position.y/4 * 3, position.width, position.height/4), property.FindPropertyRelative(nameof(StatEffect.UpperBound)));
             
+            property.FindPropertyRelative(nameof(StatEffect.Curve)).animationCurveValue = EditorGUI.CurveField(new Rect(position.x, position.y, position.width, position.height/4), property.FindPropertyRelative(nameof(StatEffect.Curve)).animationCurveValue,
+            Color.blue,
+            new Rect(0f, property.FindPropertyRelative(nameof(StatEffect.LowerBound)).floatValue, 1,
+                property.FindPropertyRelative(nameof(StatEffect.UpperBound)).floatValue -
+                property.FindPropertyRelative(nameof(StatEffect.LowerBound)).floatValue));
+
+
+
             
-           
+
+
+
         }
+
+        
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            return base.GetPropertyHeight(property, label);
+            return 300f;
         }
     }
 }
